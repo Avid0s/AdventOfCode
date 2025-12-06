6adefbd38c441b42a7678852511df2947b5ba1ac
Avid0s <gabriel.vidon@ensea.fr>
2025-12-06 19:15:25 +0100

    coding

--- AOC_1/main.c
+++ AOC_1/main.c
@@ -1,4 +1,6 @@
+#include <iso646.h>
 #include <stdio.h>
+#include <math.h>
 
 int main(void) {
     FILE *instruct;
@@ -7,7 +9,7 @@ int main(void) {
     instruct = fopen("./source/input", "r");
 
     // Print some text if the file does not exist
-    if(instruct == NULL) {
+    if (instruct == NULL) {
         printf("Not able to open the file.");
         return 1;
     }
@@ -19,25 +21,63 @@ int main(void) {
     // Store the content of the file
     char instructionText[fileSize];
 
-    // Read the content and store it inside myString
+    // Read the content and store it inside instructionText
     for (int i = 0; i < fileSize; i++) {
-
         fseek(instruct, i, SEEK_SET);
         char letter = fgetc(instruct);
         instructionText[i] = letter;
 
         // Print the file content
-        printf("%c", letter);
+        //printf("%c", letter);
     }
-
-
-
-
-
-
     // Close the file
     fclose(instruct);
 
+    bool isRight = false;
+    int size;
+    int numOfZeros = 0;
+    int position = 50;
+    for (int i = 0; i < fileSize; i++) {
+        char obsv = instructionText[i];
+        size++;
+        int prevPosition = position;
+
+        if (obsv == 'R') {
+            isRight = true;
+            size = 0;
+        } else if (obsv == 'L') {
+            isRight = false;
+            size = 0;
+        } else if (obsv == '\n') {
+            for (int j = 0; j < size - 1; j++) {
+                obsv = instructionText[i - j - 1];
+                int clicks=(obsv - '0') * pow(10, j);
+                if (isRight) {
+                    for (int k = 0; k < clicks; k++) {
+                        position ++;
+                        if (position == 100) {
+                            numOfZeros++;
+                            position -= 100;
+                        }
+                    }
+                }
+                if (!isRight) {
+                    for (int k = 0; k < clicks; k++) {
+                        position --;
+                        if (position == 0) {
+                            numOfZeros++;
+                            position += 99;
+                        }
+                    }
+                }
+            }
+
+
+
+
+        }
+        printf("%d\n", numOfZeros);
+    }
 
 
     return 0;
